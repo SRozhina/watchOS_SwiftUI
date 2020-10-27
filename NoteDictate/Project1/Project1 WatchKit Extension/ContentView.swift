96ccc0e60d5890585f4ce40dcdc83ef4ef6fafb2
Sofia Rozhina <sofia.rozhina@gmail.com>
2020-10-27 19:27:54 +0300

    Implement notes local storing and lines count flag

--- NoteDictate/Project1/Project1 WatchKit Extension/ContentView.swift
+++ NoteDictate/Project1/Project1 WatchKit Extension/ContentView.swift
@@ -4,6 +4,8 @@ struct ContentView: View {
     @State private var notes: [Note] = []
     @State private var text = ""
     
+    @AppStorage("lineCount") var lineCount = 1
+    
     var body: some View {
         VStack {
             HStack {
@@ -14,6 +16,7 @@ struct ContentView: View {
                     let note = Note(id: UUID(), text: text)
                     notes.append(note)
                     text = ""
+                    save()
                 } label: {
                     Image(systemName: "plus")
                         .padding()
@@ -32,13 +35,20 @@ struct ContentView: View {
                 ForEach(0..<notes.count, id: \.self) { i in
                     NavigationLink(destination: DetailView(index: i + 1, total: notes.count, note: notes[i])) {
                         Text(notes[i].text)
-                            .lineLimit(3)
+                            .lineLimit(lineCount)
                     }
                 }
                 .onDelete(perform: delete)
+                
+                Button("Lines: \(lineCount)") {
+                    lineCount += 1
+                    
+                    if lineCount >= 4 { lineCount = 1 }
+                }
             }
         }
         .navigationTitle("NoteDictate")
+        .onAppear(perform: load)
     }
 }
 
@@ -46,6 +56,32 @@ extension ContentView {
     func delete(offsets: IndexSet) {
         withAnimation {
             notes.remove(atOffsets: offsets)
+            save()
+        }
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(notes)
+            let url = getDocumentDirectory().appendingPathComponent("notes")
+            try data.write(to: url)
+        } catch {
+            print("Failed to write data")
+        }
+    }
+    
+    func load() {
+        DispatchQueue.main.async {
+            do {
+                let url = getDocumentDirectory().appendingPathComponent("notes")
+                let data = try Data(contentsOf: url)
+                notes = try JSONDecoder().decode([Note].self, from: data)
+            } catch { /* Do nothing */ }
         }
     }
 }
